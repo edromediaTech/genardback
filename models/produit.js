935057925a6798d5430300e4ebcd7d5aa66328a6
sironel <edromediatech@gmail.com>
2025-01-17 15:28:02 -0500

    update categorie

--- models/produit.js
+++ models/produit.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const ProduitSchema = new mongoose.Schema({
   nom: { type: String, required: true,   unique: true },
-  categorie: { type: String, enum: ['Plat', 'Boisson', 'Dessert'], required: true },
+  categorie: { type: String, enum: ['Plat', 'Boisson', 'Dessert',  'Jus naturel', 'Jus emboité',  'Boisson Alcoolisé', 'Apéritif', 'Patisserie' ], required: true },
   prix: { type: Number, required: true },
   quantite: { type: Number, default: 0 },
   ramplacement: { type: Number, default: 0 },
