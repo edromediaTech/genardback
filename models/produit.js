e8c63968eda36f2af6a4cfc7b13028e198e2ee73
sironel <edromediatech@gmail.com>
2025-03-11 21:15:24 -0400

    ajout categorie

--- models/produit.js
+++ models/produit.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const ProduitSchema = new mongoose.Schema({
   nom: { type: String, required: true,   unique: true },
-  categorie: { type: String, enum: ['Plat', 'Boisson', 'Dessert',  'Jus naturel', 'Jus emboité',  'Boisson Alcoolisé', 'Apéritif', 'Patisserie' ], required: true },
+  categorie: { type: String, enum: ['Boisson', 'Dessert',  'Jus naturel', 'Jus emboité', 
+     'Produits laitier','Produits cosmetiques','Article de nettoyage','Fuits de mer',
+    'Boisson Alcoolisé', 'Apéritif', 'Patisserie' ], required: true },
   prix: { type: Number, required: true },
   quantite: { type: Number, default: 0 },
   ramplacement: { type: Number, default: 0 },
